4373e2f652b627f53afc6da6cd7dd2aaf4730a8e
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-10-13 14:45:51 -0600

    Styles and refactor home page

---
+++ app/stats/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./stats.module.scss"
+
+export default function StatsPage() {
+	return (
+		<main className={styles.main}>
+			<h1 className={styles.main_title}>Estadísticas</h1>
+		</main>
+	)
+}
