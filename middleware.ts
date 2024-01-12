f3e005ea0f2fe22b4391e9826bcc585381da4107
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-01-12 14:22:36 -0600

    Fix middleware import and add authentication logic

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,44 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest, response: NextResponse) {
-    const session = request.cookies.get("session-soccer")
-    const { pathname } = request.nextUrl
+  const session = request.cookies.get("session-soccer")
+  const { pathname } = request.nextUrl
 
-    //Return to /login if don't have a session
-    /* if (!session) {
+  //Return to /login if don't have a session
+  /* if (!session) {
          return NextResponse.redirect(new URL("auth/login", request.url))
      }*/
 
-    //Call the authentication endpoint
+  //Call the authentication endpoint
 
+  const responseAPI = await fetch(`${"https://betting-master.vercel.app"}/api/login`, {
+    //const responseAPI = await fetch(`${"http:localhost:3000"}/api/login`, {
+    headers: {
+      Cookie: `session-soccer=${session?.value}`,
+    },
+  })
 
-    const responseAPI = await fetch(`${"https://betting-master.vercel.app"}/api/login`, {
-        headers: {
-            Cookie: `session-soccer=${session?.value}`,
-        },
-    });
+  //Return to /login if token is not authorized
+  if (responseAPI.status === 200 && pathname.endsWith("/auth/login")) {
+    request.nextUrl.pathname = "/"
+    return NextResponse.redirect(request.nextUrl)
+  }
 
+  //Return to /login if token is not authorized
+  if (responseAPI.status !== 200 && pathname.endsWith("/bets")) {
+    request.nextUrl.pathname = "/auth/login"
+    return NextResponse.redirect(request.nextUrl)
+  }
 
+  //Return to /login if token is not authorized
+  if (responseAPI.status !== 200 && pathname.endsWith("/profile")) {
+    console.log("profile")
+    request.nextUrl.pathname = "/auth/login"
+    return NextResponse.redirect(request.nextUrl)
+  }
 
-    //Return to /login if token is not authorized
-    if (responseAPI.status === 200 && pathname.endsWith("/auth/login")) {
-        request.nextUrl.pathname = "/"
-        return NextResponse.redirect(request.nextUrl)
-    }
-
-    //Return to /login if token is not authorized
-    if (responseAPI.status !== 200 && pathname.endsWith("/bets")) {
-        request.nextUrl.pathname = "/auth/login"
-        return NextResponse.redirect(request.nextUrl)
-    }
-
-    //Return to /login if token is not authorized
-    if (responseAPI.status !== 200 && pathname.endsWith("/profile")) {
-        request.nextUrl.pathname = "/auth/login"
-        return NextResponse.redirect(request.nextUrl)
-    }
-
-
-
-    /*
+  /*
         else {
             if (pathname.startsWith("/auth/login")) {
                 request.nextUrl.pathname = "/"
@@ -52,11 +48,8 @@ export async function middleware(request: NextRequest, response: NextResponse) {
         }
     */
 
-
-    //return NextResponse.next()
+  //return NextResponse.next()
 }
-
-
 
 //Add your protected routes
 
