9a99fc07772046d63aa2f8ea3fc6db00526c8f24
= <=>
2024-01-12 14:22:46 -0600

    Refactor useMatches and useWinner hooks

--- app/hooks/useMatches.ts
+++ app/hooks/useMatches.ts
@@ -3,44 +3,54 @@ import { InTimeToBet, TimeRemainig } from "../functions/functions"
 import { GetBetsByDay, GetCurrentMatchDay } from "../config/firebase"
 import { IBetDocument, IMatchDay } from "../types/types"
 
-
 export function useMatches() {
-    const [matches, setMatches] = useState<IMatchDay>({} as IMatchDay)
-    const [loading, setLoading] = useState(true)
-    //const [bets, setBets] = useState<IBetDocument[]>([])
-    const [isInTime, setIsInTime] = useState({ available: false, time: "" })
+  const [matches, setMatches] = useState<IMatchDay>({} as IMatchDay)
+  const [loading, setLoading] = useState(true)
+  //const [bets, setBets] = useState<IBetDocument[]>([])
+  const [isInTime, setIsInTime] = useState({ available: false, time: "" })
 
+  useEffect(() => {
+    GetMatches()
+  }, [])
 
-    useEffect(() => {
-        GetMatches()
-    }, [])
+  useEffect(() => {
+    if (matches.matches?.length > 0 && matches.matches[0]?.status === "Sin comenzar") {
+      const intervalRemaining = setInterval(() => {
+        const time = TimeRemainig(matches.matches[0].startDate)
+        const isTime = InTimeToBet(matches.matches[0]?.startDate)
+        setIsInTime({ available: isTime, time: time })
+      }, 1000)
 
-    useEffect(() => {
-        if (matches.matches?.length > 0 && matches.matches[0]?.status === "Sin comenzar") {
-            const intervalRemaining = setInterval(() => {
-                const time = TimeRemainig(matches.matches[0].startDate)
-                const isTime = InTimeToBet(matches.matches[0]?.startDate)
-                setIsInTime({ available: isTime, time: time })
-            }, 1000)
-
-            return () => clearInterval(intervalRemaining)
-        }
-    }, [matches])
-
-    const GetMatches = async () => {
-        const result = await GetCurrentMatchDay("0159")
-        setMatches(result)
-        setLoading(false)
-        //const documents = await GetBetsByDay(result.day.toString())
-        //setBets(documents)
-        const isTime = InTimeToBet(result.matches[0].startDate)
-        setIsInTime({ ...isInTime, available: isTime })
+      return () => clearInterval(intervalRemaining)
     }
-    return {
-        loading,
-        matches,
-        //bets,
-        isInTime,
-        //setBets,
+  }, [matches])
+
+  const GetMatches = async () => {
+    const currentMonth = new Date().getMonth() + 1
+    const tournament = currentMonth < 8 ? "0168" : "0159"
+    const result = await GetCurrentMatchDay(tournament)
+    console.log(result)
+    if (result === undefined) {
+      const emptyMatches: IMatchDay = {
+        day: 0,
+        matches: [],
+        tournament: "",
+        results: [],
+      }
+      setMatches(emptyMatches)
+      setIsInTime({ available: false, time: "" })
+    } else {
+      setMatches(result)
+      const isTime = InTimeToBet(result.matches[0].startDate)
+      setIsInTime({ ...isInTime, available: isTime })
     }
+    setLoading(false)
+  }
+  return {
+    loading,
+    matches,
+    //bets,
+    isInTime,
+    //setBets,
+  }
 }
