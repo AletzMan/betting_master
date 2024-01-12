5ce02e459bde62666e80373f2946b73fe25475b9
= <=>
2024-01-12 14:23:13 -0600

    Refactor login route to handle session cookies

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,61 +1,62 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
 import { cookies, headers } from "next/headers"
-import { auth } from "firebase-admin";
-import { InitApp } from "@/app/config/firebase-admin";
+import { auth } from "firebase-admin"
+import { InitApp } from "@/app/config/firebase-admin"
 
 InitApp()
 
 export async function POST(request: NextRequest) {
-    const authorization = headers().get("Authorization")
+  const authorization = headers().get("Authorization")
 
-    if (authorization?.startsWith("Bearer ")) {
-        const idToken = authorization.split("Bearer ")[1]
-        const decodedToken = await auth().verifyIdToken(idToken)
+  if (authorization?.startsWith("Bearer ")) {
+    const idToken = authorization.split("Bearer ")[1]
+    const decodedToken = await auth().verifyIdToken(idToken)
 
-        if (decodedToken) {
-            //Generar session cookie
-            const expiresIn = 60 * 60 * 24 * 5 * 1000
-            const sessionCookie = await auth().createSessionCookie(idToken, {
-                expiresIn
-            })
-            const options = {
-                name: "session-soccer",
-                value: sessionCookie,
-                maxAge: expiresIn,
-                httpOnly: true,
-                secure: true
-            }
+    if (decodedToken) {
+      //Generar session cookie
+      const expiresIn = 60 * 60 * 24 * 5 * 1000
+      const sessionCookie = await auth().createSessionCookie(idToken, {
+        expiresIn,
+      })
+      const options = {
+        name: "session-soccer",
+        value: sessionCookie,
+        maxAge: expiresIn,
+        httpOnly: true,
+        secure: true,
+      }
 
-            cookies().set(options)
-        }
+      cookies().set(options)
+      console.log("POST", options)
     }
-
-    return NextResponse.json({}, { status: 200 })
+  }
+  return NextResponse.json({}, { status: 200 })
 }
 
 export async function GET(request: NextRequest) {
-    const session = cookies().get("session-soccer")?.value || ""
+  const session = cookies().get("session-soccer")?.value || ""
 
-    //Validate if the cookie exist
-    if (!session) {
-        return NextResponse.json({ isLogged: false }, { status: 401 })
-    }
+  //Validate if the cookie exist
+  if (!session) {
+    return NextResponse.json({ isLogged: false }, { status: 401 })
+  }
 
-    //Validate the session cookie
-    const decodedSession = await auth().verifySessionCookie(session, true)
+  //Validate the session cookie
+  const decodedSession = await auth().verifySessionCookie(session, true)
 
-    if (!decodedSession) {
-        return NextResponse.json({ isLogged: false }, { status: 401 })
-    }
+  if (!decodedSession) {
+    return NextResponse.json({ isLogged: false }, { status: 401 })
+  }
 
-
-    return NextResponse.json(
-        {
-            isLogged: true,
-            userInfo: {
-                uid: decodedSession.uid,
-                name: decodedSession.name,
-                photo: decodedSession.picture
-            }
-        }, { status: 200 })
+  return NextResponse.json(
+    {
+      isLogged: true,
+      userInfo: {
+        uid: decodedSession.uid,
+        name: decodedSession.name,
+        photo: decodedSession.picture,
+      },
+    },
+    { status: 200 }
+  )
 }
