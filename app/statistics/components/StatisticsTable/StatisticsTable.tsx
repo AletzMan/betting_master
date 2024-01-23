3baf6e4343014fc6faab8671c8fcb42e1991b7ec
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-01-23 00:25:07 -0600

    Update API endpoint in GetStatistics function

--- app/statistics/components/StatisticsTable/StatisticsTable.tsx
+++ app/statistics/components/StatisticsTable/StatisticsTable.tsx
@@ -5,10 +5,10 @@ import { STATISTICS_OPTIONS } from "@/app/constants/constants"
 
 const GetStatistics = async (tournament: string, type: string) => {
 	try {
-		const response = await axios.get<IStatistics>(`https://betting-master.vercel.app/api/teams/statistics/${tournament}/${type}`)
+		const response = await axios.get<IStatistics>(process.env.NODE_ENV === "development" ? `http://localhost:3000/api/teams/statistics/${tournament}/${type}` : `https://betting-master.vercel.app/api/teams/statistics/${tournament}/${type}`)
 
 		if (response.status === 200) {
-			setTimeout(() => {}, 3000)
+			setTimeout(() => { }, 3000)
 			return response.data.data
 		}
 	} catch (error) {
