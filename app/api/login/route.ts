9cd6f3a81d0aa4931a5daa7e212488c8cde5d415
= <=>
2024-01-26 13:07:23 -0600

    Add error handling for session verification

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from "next/server"
 import { cookies, headers } from "next/headers"
 import { auth } from "firebase-admin"
 import { InitApp } from "@/app/config/firebase-admin"
+import { FirebaseError } from "firebase/app"
+import { AuthCredential } from "firebase/auth"
+import { ErrorFactory } from "@firebase/util"
+import { FirestoreError } from "firebase/firestore"
 
 InitApp()
 
@@ -37,30 +41,38 @@ export async function POST(request: NextRequest) {
 }
 
 export async function GET(request: NextRequest) {
-	const session = cookies().get("session-soccer")?.value || ""
+	try {
+		const session = cookies().get("session-soccer")?.value || ""
 
-	//Validate if the cookie exist
-	if (!session) {
-		return NextResponse.json({ isLogged: false }, { status: 401 })
-	}
+		//Validate if the cookie exist
+		if (!session) {
+			return NextResponse.json({ isLogged: false }, { status: 401 })
+		}
 
-	//Validate the session cookie
-	const decodedSession = await auth().verifySessionCookie(session, true)
+		//Validate the session cookie
+		const decodedSession = await auth().verifySessionCookie(session, true)
 
-	if (!decodedSession) {
-		return NextResponse.json({ isLogged: false }, { status: 401 })
-	}
+		if (!decodedSession) {
+			return NextResponse.json({ isLogged: false }, { status: 401 })
+		}
 
-	return NextResponse.json(
-		{
-			isLogged: true,
-			userInfo: {
-				uid: decodedSession.uid,
-				name: decodedSession.name,
-				photo: decodedSession.picture,
-				email: decodedSession.email,
+		return NextResponse.json(
+			{
+				isLogged: true,
+				userInfo: {
+					uid: decodedSession.uid,
+					name: decodedSession.name,
+					photo: decodedSession.picture,
+					email: decodedSession.email,
+				},
 			},
-		},
-		{ status: 200 }
-	)
+			{ status: 200 }
+		)
+	} catch (error) {
+		console.log(error instanceof Error)
+		if (error instanceof Error) {
+			cookies().delete("session-soccer")
+			return NextResponse.json({ isLogged: false, message: error.message }, { status: 401 })
+		}
+	}
 }
