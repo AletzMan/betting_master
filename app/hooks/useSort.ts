11bd7186c37e02b1ca34b112e1d181d2efff8261
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-01-31 03:23:29 -0600

    Refactor useSort hook to improve sorting functionality

--- app/hooks/useSort.ts
+++ app/hooks/useSort.ts
@@ -2,41 +2,61 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import { SortByHits } from "../functions/functions"
 import { IBetDocument, IUserAndState } from "../types/types"
 
+export function useSort(
+	results: string[],
+	bets: IBetDocument[],
+	setBets: Dispatch<SetStateAction<IBetDocument[]>>
+) {
+	const [orderBets, setOrderBets] = useState<"name" | "asc" | "des" | "normal" | "">("")
 
-export function useSort(results: string[], bets: IBetDocument[], setBets: Dispatch<SetStateAction<IBetDocument[]>>) {
-    const [orderBets, setOrderBets] = useState({ id: "name", name: "Por nombre" })
+	useEffect(() => {
+		if (bets.length > 0) {
+			if (orderBets === "asc") {
+				const newOrder = SortByHits("asc", bets, results)
+				setBets(newOrder)
+			} else if (orderBets === "des") {
+				const newOrder = SortByHits("des", bets, results)
+				setBets(newOrder)
+			} else if (orderBets === "name") {
+				SortByName()
+			} else if (orderBets === "normal") {
+				SortNormal()
+			}
+		}
+	}, [orderBets])
 
-    useEffect(() => {
-        if (bets.length > 0) {
-            if (orderBets.id === "asc") {
-                const newOrder = SortByHits("asc", bets, results)
-                setBets(newOrder)
-            } else if (orderBets.id === "des") {
-                const newOrder = SortByHits("des", bets, results)
-                setBets(newOrder)
-            } else if (orderBets.id === "name") {
-                SortByName()
-            }
-        }
-    }, [orderBets])
+	const SortByName = () => {
+		let newOrder = [...bets]
+		newOrder?.sort((a, b) => {
+			if (a.name > b.name) {
+				return 1
+			}
+			if (a.name < b.name) {
+				return -1
+			}
+			// a must be equal to b
+			return 0
+		})
+		setBets(newOrder)
+	}
 
-    const SortByName = () => {
-        let newOrder = [...bets]
-        newOrder?.sort((a, b) => {
-            if (a.name > b.name) {
-                return 1
-            }
-            if (a.name < b.name) {
-                return -1
-            }
-            // a must be equal to b
-            return 0
-        })
-        setBets(newOrder)
-    }
+	const SortNormal = () => {
+		let newOrder = [...bets]
+		newOrder?.sort((a, b) => {
+			if (a.uid > b.uid) {
+				return -1
+			}
+			if (a.uid < b.uid) {
+				return 1
+			}
+			// a must be equal to b
+			return 0
+		})
+		setBets(newOrder)
+	}
 
-    return {
-        orderBets,
-        setOrderBets
-    }
+	return {
+		orderBets,
+		setOrderBets,
+	}
 }
