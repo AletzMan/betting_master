32aafbd42101b4450521f37cf2b30fb25465b9e2
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-02-05 22:52:18 -0600

    Update Loading component with new icon and color

--- app/components/Loading/Loading.tsx
+++ app/components/Loading/Loading.tsx
@@ -1,10 +1,10 @@
-import { LoadingIcon } from "@/app/svg"
+import { LoadingIcon, LoadingTwoIcon } from "@/app/svg"
 import styles from "./loading.module.scss"
 
 export function Loading() {
 	return (
 		<div className={styles.loading}>
-			<LoadingIcon className={styles.loading_icon} />
+			<LoadingTwoIcon className={styles.loading_icon} />
 			<span className={styles.loading_text}>Cargando</span>
 		</div>
 	)
