b4213e80331497729075002b2231a18ad37c270c
= <=>
2024-05-08 19:31:01 -0600

    ReactorL add data firebase

--- app/profile/AdminFinals/AdminFinals.tsx
+++ app/profile/AdminFinals/AdminFinals.tsx
@@ -5,7 +5,7 @@ import { IParticipants } from "@/app/types/types"
 import { QualifiedTeams } from "@/app/finals/components/Quarterfinals"
 import { Button } from "@/app/components/Button/Button"
 import { LuckIcon } from "@/app/svg"
-import { UpdateFinalParticipants } from "@/app/config/firebase"
+import { GetFinalParticipants, UpdateFinalParticipants } from "@/app/config/firebase"
 
 export default function AdminFinals() {
     const { enqueueSnackbar } = useSnackbar()
@@ -53,105 +53,8 @@ export default function AdminFinals() {
     }
 
     const GetParticipants = async () => {
-        //const data = await GetFinalParticipants()
-        const data = [
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocJ7E4PzlEjRsIqGpdN71kUe-sG33WN9UXMQ61Wpp0oVP2U=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Alejandro Garcia'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocJV7zokRNBsZLofjwaVPe9HJrOz487ZQduUXf6n3bO7=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Judith Garcia'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocLZzEGfe6SOAX62JGMV4TKbreD5d0lkuA6Pidwx7Mt2=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'JK Garcia'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocJHdeCqmsWqnlERyNQu_m-MLBuqe6FVeLSsJSMM4Efr=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Paty Garcia'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocLA92MJoEylcI8tY0R26cCh9ok-aZdPN1UOMty4ESIJ=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Ma del Rosario'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocKulEAaKcIXyW5CAeLySco3xBveoDj0prmY8LMeQ0IP=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Juan Galvan'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocInVz5eFzExt4oD9KmQjLH_akceoyNSIPbbbzcnSYsW=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Aide Alonso'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            },
-            {
-                team: '',
-                uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                userInfo: {
-                    photo:
-                        'https://lh3.googleusercontent.com/a/ACg8ocJHvPhVKJiHO1-lxgi0jGlJnSlIbGY43RpR0EFCIhzI=s96-c',
-                    email: 'alejo2986@gmail.com',
-                    uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
-                    name: 'Lola Torres'
-                },
-                id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
-            }
-        ]
+        const data = await GetFinalParticipants()
+
         if (data.length > 0) {
             setParticipants(data)
         } else {
@@ -166,3 +69,103 @@ export default function AdminFinals() {
         </article>
     )
 }
+
+/*const data = [
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocJ7E4PzlEjRsIqGpdN71kUe-sG33WN9UXMQ61Wpp0oVP2U=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Alejandro Garcia'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocJV7zokRNBsZLofjwaVPe9HJrOz487ZQduUXf6n3bO7=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Judith Garcia'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocLZzEGfe6SOAX62JGMV4TKbreD5d0lkuA6Pidwx7Mt2=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'JK Garcia'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocJHdeCqmsWqnlERyNQu_m-MLBuqe6FVeLSsJSMM4Efr=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Paty Garcia'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocLA92MJoEylcI8tY0R26cCh9ok-aZdPN1UOMty4ESIJ=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Ma del Rosario'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocKulEAaKcIXyW5CAeLySco3xBveoDj0prmY8LMeQ0IP=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Juan Galvan'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocInVz5eFzExt4oD9KmQjLH_akceoyNSIPbbbzcnSYsW=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Aide Alonso'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           },
+           {
+               team: '',
+               uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+               userInfo: {
+                   photo:
+                       'https://lh3.googleusercontent.com/a/ACg8ocJHvPhVKJiHO1-lxgi0jGlJnSlIbGY43RpR0EFCIhzI=s96-c',
+                   email: 'alejo2986@gmail.com',
+                   uid: 'AGstpoT4F9WHdWIwZjbHP6TRHea2',
+                   name: 'Lola Torres'
+               },
+               id: '544684cf-ccfe-4946-95ec-09dfcf137c53'
+           }
+       ]
+       */
