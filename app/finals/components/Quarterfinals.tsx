170d4564bdb7a25235782b62338e01bd87227d1f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-12-19 00:06:33 -0600

    style: Update quarters module styles for SVG icons

--- app/finals/components/Quarterfinals.tsx
+++ app/finals/components/Quarterfinals.tsx
@@ -1,7 +1,21 @@
-import { AmericaLogo, ChivasLogo, CruzAzulLogo, MonterreyLogo, NecaxaLogo, PachucaLogo, PumasLogo, SanLuisLogo, TigresLogo, TijuanaLogo, TolucaLogo } from "../../svg"
+import { GetFinalistTeams } from "@/app/config/firebase"
 import styles from "./quarters.module.scss"
+import { useEffect, useState } from "react"
+import { TeamsLocalLogos } from "@/app/constants/constants"
 
 export default function QuarterFinals() {
+    const [qualifiedTeams, setQualifiedTeams] = useState<string[]>([])
+
+    useEffect(() => {
+        GetQualifiedTeams()
+    }, [])
+
+    const GetQualifiedTeams = async () => {
+        const teams = await GetFinalistTeams()
+        if (teams.positions.length > 0) {
+            setQualifiedTeams(teams.positions)
+        }
+    }
 
     return (
         <>
@@ -10,31 +24,31 @@ export default function QuarterFinals() {
                 <div className={styles.matches_container}>
                     <article className={styles.matches_match}>
                         <div className={styles.team}>
-                            {QualifiedTeams[6].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[0])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[0].name}
+                                {qualifiedTeams[0]}
                             </span>
                         </div>
                         <span className={styles.vs}>VS</span>
                         <div className={styles.team}>
-                            {QualifiedTeams[7].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[7])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[7].name}
+                                {qualifiedTeams[7]}
                             </span>
                         </div>
                     </article>
                     <article className={styles.matches_match}>
                         <div className={styles.team}>
-                            {QualifiedTeams[1].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[1])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[1].name}
+                                {qualifiedTeams[1]}
                             </span>
                         </div>
                         <span className={styles.vs}>VS</span>
                         <div className={styles.team}>
-                            {QualifiedTeams[6].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[6])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[6].name}
+                                {qualifiedTeams[6]}
                             </span>
                         </div>
                     </article>
@@ -42,31 +56,31 @@ export default function QuarterFinals() {
                 <div className={styles.matches_container}>
                     <article className={styles.matches_match}>
                         <div className={styles.team}>
-                            {QualifiedTeams[2].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[2])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[2].name}
+                                {qualifiedTeams[2]}
                             </span>
                         </div>
                         <span className={styles.vs}>VS</span>
                         <div className={styles.team}>
-                            {QualifiedTeams[5].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[5])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[5].name}
+                                {qualifiedTeams[5]}
                             </span>
                         </div>
                     </article>
                     <article className={styles.matches_match}>
                         <div className={styles.team}>
-                            {QualifiedTeams[3].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[3])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[3].name}
+                                {qualifiedTeams[3]}
                             </span>
                         </div>
                         <span className={styles.vs}>VS</span>
                         <div className={styles.team}>
-                            {QualifiedTeams[4].logo}
+                            {TeamsLocalLogos.find(team => team.name === qualifiedTeams[4])?.logo}
                             <span className={styles.team_name}>
-                                {QualifiedTeams[4].name}
+                                {qualifiedTeams[4]}
                             </span>
                         </div>
                     </article>
@@ -76,78 +90,3 @@ export default function QuarterFinals() {
     )
 }
 
-
-export const QualifiedTeams = [
-    {
-        id: 1,
-        logo: <CruzAzulLogo className={styles.logo} />,
-        name: "Cruz Azul",
-        abbName: "CRZ",
-        color_one: "#001f60",
-        color_two: "#FFFFFF",
-        color_three: "#de1d33",
-    },
-    {
-        id: 2,
-        logo: <TolucaLogo className={styles.logo} />,
-        name: "Toluca",
-        abbName: "TOL",
-        color_one: "#ce0e2d",
-        color_two: "#FFFFFF",
-        color_three: "#111c4e",
-    },
-    {
-        id: 3,
-        logo: <TigresLogo className={styles.logo} />,
-        name: "Tigres",
-        abbName: "TIG",
-        color_one: "#f1a21e",
-        color_two: "#FFFFFF",
-        color_three: "#00369c",
-    },
-    {
-        id: 4,
-        logo: <PumasLogo className={styles.logo} />,
-        name: "Pumas",
-        abbName: "PUM",
-        color_one: "#001e47",
-        color_two: "#FFFFFF",
-        color_three: "#766910",
-    },
-    {
-        id: 5,
-        logo: <MonterreyLogo className={styles.logo} />,
-        name: "Monterrey",
-        abbName: "MTY",
-        color_one: "#0a2240",
-        color_two: "#FFFFFF",
-        color_three: "#FFFFFF",
-    },
-    {
-        id: 6,
-        logo: <SanLuisLogo className={styles.logo} />,
-        name: "San Luis",
-        abbName: "SLUIS",
-        color_one: "#CE0E2D",
-        color_two: "#FFFFFF",
-        color_three: "#002856",
-    },
-    {
-        id: 7,
-        logo: <AmericaLogo className={styles.logo} />,
-        name: "América",
-        abbName: "AME",
-        color_one: "#d6bf25",
-        color_two: "#00225d",
-        color_three: "#e4503a",
-    },
-    {
-        id: 8,
-        logo: <TijuanaLogo className={styles.logo} />,
-        name: "Tijuana",
-        abbName: "TIJ",
-        color_one: "#CE0E2D",
-        color_two: "#111c4e",
-        color_three: "#FFFFFF",
-    },
-]
