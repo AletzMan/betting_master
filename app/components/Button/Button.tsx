a25308999ce94506fe92c9a05873962f13ac6a04
= <=>
2024-12-21 02:46:40 -0600

    refactor: Update Button component to accept className prop

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,19 +1,20 @@
-import { DOMAttributes, ButtonHTMLAttributes } from 'react'
+import { DOMAttributes, ButtonHTMLAttributes, HTMLAttributes } from 'react'
 import styles from './button.module.scss'
 
 interface Props {
-    props: DOMAttributes<HTMLButtonElement> | ButtonHTMLAttributes<HTMLButtonElement>
+    props: DOMAttributes<HTMLButtonElement> | ButtonHTMLAttributes<HTMLButtonElement> | HTMLAttributes<HTMLButtonElement>
     icon?: React.ReactNode
     text?: string
     type?: "primary" | "secondary" | "error" | "success"
+    className?: string
 }
 
-export function Button({ icon, text, type, props }: Props) {
+export function Button({ icon, text, type, props, className }: Props) {
 
     return (
         <button
             {...props}
-            className={`${styles.button} ${type === "secondary" && styles.button_secondary}  ${type === "error" && styles.button_error} ${type === "success" && styles.button_success} `}
+            className={`${styles.button} ${type === "secondary" ? styles.button_secondary : ""}  ${type === "error" ? styles.button_error : ""} ${type === "success" ? styles.button_success : ""} ${className}`}
         >
             {icon}
             {text}
