c0c2df3baf2b61ff61fc0dde01a09928abd570c3
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2024-12-23 11:21:18 -0600

    chore: Refactor profile page to optimize rendering and remove unnecessary code

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,5 @@
 
+"use client"
 import styles from "./profile.module.scss"
 import { AdminPanel } from "./AdminPanel/AdminPanel"
 import { SettingsProfile } from "./Settings/SettingsProfile"
@@ -7,20 +8,22 @@ import AdminFinals from "./AdminFinals/AdminFinals"
 import { AppConfig } from "./AppConfig/AppConfig"
 import { AdminNotifications } from "./AdminNotifications/AdminNotifications"
 import { GetCurrentMatchDay, GetInfoUser } from "../config/firebase"
+import { useUser } from "../config/zustand-store"
 
 const ADMIN_UID = process.env.NEXT_PUBLIC_ADMIN_UID
 
-export default async function ProfilePage() {
+export default function ProfilePage() {
+	const { user } = useUser()
 
 	return (
 		<main className={`${styles.main}   scrollbar`}>
 			<section className={styles.section}>
 				{<SettingsProfile />}
-				{loading && user.uid === ADMIN_UID && <AppConfig />}
-				{loading && user.uid === ADMIN_UID && <AdminPanel />}
-				{loading && user.uid === ADMIN_UID && <AdminNotifications />}
-				{loading && user.uid === ADMIN_UID && <BetsByUser />}
-				{loading && user.uid === ADMIN_UID && <AdminFinals />}
+				{user.uid === ADMIN_UID && <AppConfig />}
+				{user.uid === ADMIN_UID && <AdminPanel />}
+				{user.uid === ADMIN_UID && <AdminNotifications />}
+				{user.uid === ADMIN_UID && <BetsByUser />}
+				{user.uid === ADMIN_UID && <AdminFinals />}
 			</section>
 		</main>
 	)
