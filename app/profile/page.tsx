6e403c23f087a29ff1aec9d4b83e257326f18c2d
= <=>
2024-12-24 01:40:48 -0600

    Refactor profile page to remove unused imports and variables

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,7 +7,6 @@ import { BetsByUser } from "./BetsByUser/BetsByUser"
 import AdminFinals from "./AdminFinals/AdminFinals"
 import { AppConfig } from "./AppConfig/AppConfig"
 import { AdminNotifications } from "./AdminNotifications/AdminNotifications"
-import { GetCurrentMatchDay, GetInfoUser } from "../config/firebase"
 import { useUser } from "../config/zustand-store"
 
 const ADMIN_UID = process.env.NEXT_PUBLIC_ADMIN_UID
