75de42dc7bc1550d1efc1aecf64face9c5fbf6fb
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-21 00:59:07 -0600

    chore: Add disabled prop to Button component

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -6,15 +6,17 @@ interface Props {
     icon?: React.ReactNode
     text?: string
     type?: "primary" | "secondary" | "error" | "success"
+    disabled?: boolean
     className?: string
 }
 
-export function Button({ icon, text, type, props, className }: Props) {
+export function Button({ icon, text, type, props, className, disabled }: Props) {
 
     return (
         <button
             {...props}
-            className={`${styles.button} ${type === "secondary" ? styles.button_secondary : ""}  ${type === "error" ? styles.button_error : ""} ${type === "success" ? styles.button_success : ""} ${className}`}
+            disabled={disabled}
+            className={`${styles.button}   ${type === "secondary" ? styles.button_secondary : ""}  ${type === "error" ? styles.button_error : ""} ${type === "success" ? styles.button_success : ""} ${className}`}
         >
             {icon}
             {text}
