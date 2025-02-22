1360483e31053f30ddbd1b05ed62cb0dc02e1e18
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-22 00:19:28 -0600

    chore: Update primary color in globals.css, remove unused imports and components, update PrimeReact theme and styles, and add tab navigation

--- app/profile/AdminNotifications/AdminNotifications.tsx
+++ app/profile/AdminNotifications/AdminNotifications.tsx
@@ -1,18 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
 import styles from "./styles.module.scss"
-import stylesGeneral from "../profile.module.scss"
-import { AdminIcon, ArrowUpIcon, DeleteIcon, EmailIcon, LoadingIcon, NotificationIcon, RefreshIcon, SendIcon } from "@/app/svg"
-import { Button } from "@/app/components/Button/Button"
+import { DeleteIcon, EmailIcon, NotificationIcon } from "@/app/svg"
 import { MouseEvent, useState } from "react"
 import { DeleteUser, GetCurrentMatchDay, GetUsers, UpdateNotificationUser } from "@/app/config/firebase"
-import axios from "axios"
 import { SendNotifications } from "@/app/services/fetch_utils"
 import { IUserSettings } from "@/app/types/types"
-import { FormattedDateUpcoming, SmallDateLocal } from "@/app/utils/helpers"
+import { SmallDateLocal } from "@/app/utils/helpers"
 import { enqueueSnackbar } from "notistack"
-import { useMatches } from "@/app/hooks/useMatches"
-import Details from "@/app/components/Details/Details"
+import { Button } from "primereact/button"
+import Image from "next/image"
 
 
 export function AdminNotifications() {
@@ -76,59 +73,54 @@ export function AdminNotifications() {
             setUsersData(responseUsers)
         }
     }
-    console.log(usersData)
-
     return (
-        <Details name="adminpanel" title="Notificaciones y usuarios" icon={<NotificationIcon className="" />} >
-            <div className={styles.options}>
-                <header className={styles.header}>
-                    <button className={styles.header_button} onClick={HandleRefreshUsers}>
-                        <RefreshIcon className={styles.header_icon} />
+
+        <div className="flex flex-col gap-2 relative h-[calc(100svh-9rem)] ">
+            <header className="flex items-center justify-end pb-1">
+                <Button onClick={HandleRefreshUsers} icon="pi pi-refresh" severity="success" size="small" outlined label="Actualizar" />
+            </header>
+            <div className="flex flex-col gap-1 scrollbar">
+                {usersData.map((user, index) => (
+                    <button className={`${styles.users_user} ${viewDetails === index && styles.users_userActive}`} key={user.uid} onClick={(e) => HandleViewDetails(e, index)}>
+                        <div className={styles.users_data} >
+                            <Image className={styles.users_photo} src={user.photo || ""} alt={`Imagen de perfil de ${user.name}`} width={70} height={70} />
+                            <div className={styles.users_group} >
+                                <span className={styles.users_name}>{user.name}</span>
+                                <span className={styles.users_email}>{user.email}</span>
+                            </div>
+                            <span className={styles.users_date}>{new Date(user.last_login).toLocaleDateString("es-MX", SmallDateLocal)}</span>
+                        </div>
+                        <div className={styles.users_stats}>
+                            <div className={styles.users_bets}>
+                                <span className={styles.users_number}>{user.total_bets}</span>
+                                <span className={styles.users_label}>quinielas</span>
+                            </div>
+                            <div className={styles.users_bets}>
+                                <span className={styles.users_number}>{user.bets_won}</span>
+                                <span className={styles.users_label}>Ganadoras</span>
+                            </div>
+                            <div className={styles.users_bets}>
+                                <span className={styles.users_number}>{user.finals_won}</span>
+                                <span className={styles.users_label}>Finales</span>
+                            </div>
+                        </div>
+                        <div className={styles.users_options}>
+                            <button className={styles.users_mail} onClick={() => HandleUpdateNotification(user.uid, user.notifications)}><NotificationIcon className={`${styles.users_iconNoti} ${user.notifications && styles.users_iconNotiActive}`} /></button>
+                            <button className={styles.users_mail} > <EmailIcon className={styles.users_iconEmail} /></button>
+                            <button className={styles.users_delete} onClick={() => HandleDeleteUser(user.uid)}><DeleteIcon className={styles.users_iconDelete} /></button>
+                        </div>
                     </button>
-                </header>
-                <div className={styles.users}>
-                    {usersData.map((user, index) => (
-                        <button className={`${styles.users_user} ${viewDetails === index && styles.users_userActive}`} key={user.uid} onClick={(e) => HandleViewDetails(e, index)}>
-                            <div className={styles.users_data} >
-                                <img className={styles.users_photo} src={user.photo || ""} alt={`Imagen de perfil de ${user.name}`} />
-                                <div className={styles.users_group} >
-                                    <span className={styles.users_name}>{user.name}</span>
-                                    <span className={styles.users_email}>{user.email}</span>
-                                </div>
-                                <span className={styles.users_date}>{new Date(user.last_login).toLocaleDateString("es-MX", SmallDateLocal)}</span>
-                            </div>
-                            <div className={styles.users_stats}>
-                                <div className={styles.users_bets}>
-                                    <span className={styles.users_number}>{user.total_bets}</span>
-                                    <span className={styles.users_label}>quinielas</span>
-                                </div>
-                                <div className={styles.users_bets}>
-                                    <span className={styles.users_number}>{user.bets_won}</span>
-                                    <span className={styles.users_label}>Ganadoras</span>
-                                </div>
-                                <div className={styles.users_bets}>
-                                    <span className={styles.users_number}>{user.finals_won}</span>
-                                    <span className={styles.users_label}>Finales</span>
-                                </div>
-                            </div>
-                            <div className={styles.users_options}>
-                                <button className={styles.users_mail} onClick={() => HandleUpdateNotification(user.uid, user.notifications)}><NotificationIcon className={`${styles.users_iconNoti} ${user.notifications && styles.users_iconNotiActive}`} /></button>
-                                <button className={styles.users_mail} > <EmailIcon className={styles.users_iconEmail} /></button>
-                                <button className={styles.users_delete} onClick={() => HandleDeleteUser(user.uid)}><DeleteIcon className={styles.users_iconDelete} /></button>
-                            </div>
-                            {/*<input className={styles.users_checkbox} type="checkbox" defaultChecked={user.notifications} />*/}
-                        </button>
-                    ))}
-                </div>
-                <footer className={styles.footer}>
-                    {<Button
-                        props={{ onClick: HandleSendNotifications, disabled: sending }}
-                        text={!sending ? "Enviar notificación" : "Sending..."}
-                        icon={sending ? <LoadingIcon className={styles.admin_updateIcon} /> : <SendIcon className={styles.admin_updateIcon} />}
-                        type="primary"
-                    />}
-                </footer>
+                ))}
             </div>
-        </Details>
+            <footer className="flex min-h-12 items-center pt-2">
+                <Button label={!sending ? "Enviar notificación" : "Sending..."} size="small" icon={sending ? "pi pi-spin pi-spinner-dotted" : "pi pi-send"} disabled={sending} />
+                {/*<Button
+                    props={{ onClick: HandleSendNotifications, disabled: sending }}
+                    text={!sending ? "Enviar notificación" : "Sending..."}
+                    icon={sending ? <LoadingIcon className={styles.admin_updateIcon} /> : <SendIcon className={styles.admin_updateIcon} />}
+                    type="primary"
+            />*/}
+            </footer>
+        </div>
     )
 }
