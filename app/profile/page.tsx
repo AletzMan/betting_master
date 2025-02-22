d8d6200bb96358bd78bc53842d65c9b5d04d3bc0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-21 18:55:04 -0600

    chore: Update max-width and background color in globals.css, update PrimeReact theme in layout.tsx, and add tab navigation in profile/page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,6 +8,8 @@ import AdminFinals from "./AdminFinals/AdminFinals"
 import { AppConfig } from "./AppConfig/AppConfig"
 import { AdminNotifications } from "./AdminNotifications/AdminNotifications"
 import { useUser } from "../config/zustand-store"
+import { TabPanel, TabView } from "primereact/tabview"
+import { AdminIcon, ProfileIcon } from "../svg"
 
 const ADMIN_UID = process.env.NEXT_PUBLIC_ADMIN_UID
 
@@ -16,14 +18,31 @@ export default function ProfilePage() {
 
 	return (
 		<main className={`sectionContainer scrollbar`}>
-			<section className="flex flex-col gap-2 w-full max-w-125 p-2 border-1 border-(--surface-d) rounded-b-md bg-(--surface-e)">
+			{/*<section className="flex flex-col gap-2 w-full max-w-125 p-2 border-1 border-(--surface-d) rounded-b-md bg-(--surface-e)">
 				{<SettingsProfile />}
 				{user.uid === ADMIN_UID && <AppConfig />}
 				{user.uid === ADMIN_UID && <AdminPanel />}
 				{user.uid === ADMIN_UID && <AdminNotifications />}
 				{user.uid === ADMIN_UID && <BetsByUser />}
 				{user.uid === ADMIN_UID && <AdminFinals />}
-			</section>
+	</section>*/}
+			<TabView className="w-full">
+				<TabPanel header="Perfil" leftIcon="pi pi-user mr-2">
+					<SettingsProfile />
+				</TabPanel>
+				<TabPanel header="Ajustes" leftIcon="pi pi-cog  mr-2">
+					<AppConfig />
+				</TabPanel>
+				<TabPanel header="Notificaciones" leftIcon="pi pi-bell  mr-2">
+					<AdminNotifications />
+				</TabPanel>
+				<TabPanel header="Pagos" leftIcon="pi pi-dollar  mr-2">
+					<BetsByUser />
+				</TabPanel>
+				<TabPanel header="Sorteo" leftIcon="pi pi-gift  mr-2">
+					<AdminFinals />
+				</TabPanel>
+			</TabView>
 		</main>
 	)
 }
