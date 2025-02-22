036bf9f3e11ed2b59ffbf9397d9dccec3b47bd7d
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-22 13:10:03 -0600

    feat: Add API route for retrieving users and creating new users

---
+++ app/api/users/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/app/utils/db";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { ConflictError, DefaultError, ServerError, UnprocessableEntityError } from "../_services/errors";
+import { SuccessCreate, SuccessResponse } from "../_services/successfulResponses";
+import { UserSchema } from "@/app/validations/userSchema";
+import { ZodError } from "zod";
+
+export async function GET(request: NextRequest) {
+
+    const response = await prisma?.user.findMany()
+
+    return SuccessResponse(response);
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const data = await request.json()
+        const newUser = await UserSchema.parseAsync(data)
+
+        const response = await prisma?.user.create({
+            data: {
+                account: newUser.account,
+                bets_won: newUser.bets_won,
+                color: newUser.color,
+                email: newUser.email,
+                finals_won: newUser.finals_won,
+                last_login: newUser.last_login,
+                name: newUser.name,
+                notifications: newUser.notifications,
+                photo: newUser.photo,
+                total_bets: newUser.total_bets,
+                uid: newUser.uid
+            }
+        })
+
+        if (response) {
+            return SuccessCreate(response);
+        }
+        return DefaultError("Error al crear el registro");
+
+    } catch (error) {
+        console.log(error);
+        if (error instanceof PrismaClientKnownRequestError) {
+            if (error.code === "P2002") {
+                return ConflictError();
+            }
+        }
+        if (error instanceof ZodError) {
+            return UnprocessableEntityError(error.issues);
+        }
+        return ServerError();
+    }
+}
