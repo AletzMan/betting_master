a816b86d09060723726b5168e52bd7f2daea0ddd
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-24 22:37:00 -0600

    refactor: Update icons and styles in LinksPage component

---
+++ app/components/MenuPages.tsx
@@ -0,0 +1,113 @@
+import Link from "next/link"
+import styles from "./styles.module.scss"
+import { LogInIcon, LogOutIcon } from "@/app/svg"
+import { LinksPage } from "@/app/constants/constants"
+import { usePathname } from "next/navigation"
+import { useMenu } from "@/app/config/zustand-store"
+import { MouseEvent } from "react"
+import { useLoggedUser } from "@/app/hooks/useLoggedUser"
+import { useRouter } from "next/navigation"
+import { signOut, useSession } from "next-auth/react"
+import { Sidebar } from "primereact/sidebar"
+import { Button } from "primereact/button"
+import { Divider } from "primereact/divider"
+
+export function MenuPages() {
+    const pathname = usePathname()
+    const router = useRouter()
+    const { openMenu, setOpenMenu } = useMenu()
+    const { isLogged, setIsLogged, setUser } = useLoggedUser()
+    const session = useSession()
+
+    const HandleClickDialog = (e: MouseEvent<HTMLDialogElement>) => {
+        if ((e.target as HTMLElement).tagName === "DIALOG") {
+            setOpenMenu(false)
+        }
+    }
+
+    const HandleSignOut = async () => {
+        try {
+            await signOut({ redirect: true, redirectTo: "/" })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+
+    return (
+        <Sidebar
+            visible={openMenu}
+            onHide={() => setOpenMenu(false)}
+            content={
+                <nav >
+                    <Button
+                        label="Menu" icon="pi pi-bars"
+                        text severity="info" size="large"
+                        onClick={() => setOpenMenu(false)}>
+                    </Button>
+                    <div className="flex flex-col items-center justify-start w-full mt-8 py-1 px-4 gap-1.5">
+                        {LinksPage.map((link, index) => index == 0 && (
+                            <Link
+                                className={`flex flex-row gap-3.5 p-1.5 items-center rounded-sm hover:bg-(--surface-d) font-bold w-full ${link.pathname === pathname ? "bg-(--primary-color) ml-6 pointer-events-none text-(--surface-50) " : "transparent pointer-events-auto text-(--surface-800) "}`}
+                                key={link.id}
+                                href={link.href}
+                                title={link.title}>
+                                {link.icon}
+                                {link.text}
+                            </Link>
+                        ))}
+                        <Divider align="left"><span className="text-gray-400 font-light italic">Pronósticos</span></Divider>
+                        {LinksPage.map((link, index) => (index > 1 && index < 4) && (
+                            <Link
+                                className={`flex flex-row gap-3.5 p-1.5  items-center rounded-sm hover:bg-(--surface-d) font-bold w-full ${link.pathname === pathname ? "bg-(--primary-color) ml-6 pointer-events-none text-(--surface-50) " : "transparent pointer-events-auto text-(--surface-800) "}`}
+                                key={link.id}
+                                href={link.href}
+                                title={link.title}>
+                                {link.icon}
+                                {link.text}
+                            </Link>
+                        ))}
+                        <Divider align="left"><span className="text-gray-400 font-light italic" >Equipos</span></Divider>
+                        {LinksPage.map((link, index) => (index > 3 && index < 7) && (
+                            <Link
+                                className={`flex flex-row gap-3.5 p-1.5  items-center rounded-sm hover:bg-(--surface-d) font-bold w-full ${link.pathname === pathname ? "bg-(--primary-color) ml-6 pointer-events-none text-(--surface-50) " : "transparent pointer-events-auto text-(--surface-800) "}`}
+
+                                key={link.id}
+                                href={link.href}
+                                title={link.title}>
+                                {link.icon}
+                                {link.text}
+                            </Link>
+                        ))}
+                        <Divider align="left"><span className="text-gray-400 font-light italic" >Cuenta</span></Divider>
+                        {LinksPage.map((link, index) => index === 1 && (
+                            <Link
+                                className={`flex flex-row gap-3.5 p-1.5  items-center rounded-sm hover:bg-(--surface-d) font-bold w-full ${link.pathname === pathname ? "bg-(--primary-color) ml-6 pointer-events-none text-(--surface-50) " : "transparent pointer-events-auto text-(--surface-800) "}`}
+
+                                key={link.id}
+                                href={link.href}
+                                title={link.title}>
+                                {link.icon}
+                                {link.text}
+                            </Link>
+                        ))}
+                        {session.status === "unauthenticated" && (
+                            <Link href={!isLogged ? "/login" : "/logout"} className={`flex flex-row gap-3.5 p-1.5  items-center rounded-sm hover:bg-(--surface-d) font-bold w-full `}
+                                title={"Ir a sección iniciar sesión"}>
+                                <i className="pi pi-sign-in" style={{ fontSize: '1.25rem' }} />
+                                {"Iniciar sesión"}
+                            </Link>
+                        )}
+                        {session.status === "authenticated" && (
+                            <button onClick={HandleSignOut} className={`flex flex-row gap-3.5 p-1.5  items-center rounded-sm hover:bg-(--surface-d) font-bold w-full `}>
+                                <i className="pi pi-sign-out" style={{ fontSize: '1.25rem' }} />
+                                {"Cerrar sesión"}
+                            </button>
+                        )}
+                    </div>
+                </nav>
+            } />
+    )
+}
+
--- app/constants/constants.tsx
+++ app/constants/constants.tsx
@@ -3,35 +3,26 @@ import {
 	AtlasLogo,
 	AtleticoMardidLogo,
 	BarcelonaLogo,
-	BetsIcon,
 	ChivasLogo,
 	CruzAzulLogo,
 	GetafeLogo,
-	HomeIcon,
 	JuarezLogo,
 	LeonLogo,
-	LuckIcon,
 	MazatlanLogo,
 	MonterreyLogo,
 	NecaxaLogo,
 	OsasunaLogo,
 	PachucaLogo,
-	PositionsIcon,
-	ProfileIcon,
 	PueblaLogo,
 	PumasLogo,
 	QueretaroLogo,
 	RayoLogo,
 	RealMadridLogo,
-	ResultsIcon,
 	SanLuisLogo,
 	SantosLogo,
-	StarIcon,
-	StatsIcon,
 	TigresLogo,
 	TijuanaLogo,
 	TolucaLogo,
-	WinnerIcon,
 } from "@/app/svg"
 import { IOptions, Team } from "../types/types"
 
@@ -784,7 +775,7 @@ export const LinksPage = [
 		pathname: "/",
 		href: "/",
 		title: "Ir a pagina de inicio",
-		icon: <HomeIcon className="" />,
+		icon: <i className="pi pi-home" style={{ fontSize: '1.25rem' }} />,
 	},
 	{
 		id: 1,
@@ -792,7 +783,7 @@ export const LinksPage = [
 		pathname: "/profile",
 		href: "/profile",
 		title: "Ir a sección de perfil",
-		icon: <ProfileIcon className="" />,
+		icon: <i className="pi  pi-id-card " style={{ fontSize: '1.25rem' }} />,
 	},
 	{
 		id: 2,
@@ -800,7 +791,7 @@ export const LinksPage = [
 		pathname: "/bets",
 		href: "/bets",
 		title: "Ir a página de quinielas",
-		icon: <BetsIcon className="" />,
+		icon: <i className="pi pi-list" style={{ fontSize: '1.25rem' }} />,
 	},
 	{
 		id: 3,
@@ -808,7 +799,7 @@ export const LinksPage = [
 		pathname: "/finals",
 		href: "/finals",
 		title: "Ir a enfrentamientos finales",
-		icon: <WinnerIcon className="" />,
+		icon: <i className="pi pi-trophy" style={{ fontSize: '1.25rem' }} />,
 	},
 	{
 		id: 4,
@@ -816,7 +807,7 @@ export const LinksPage = [
 		pathname: "/results",
 		href: "/results",
 		title: "Ir a página de resultados",
-		icon: <ResultsIcon className="" />,
+		icon: <i className="pi pi-receipt" style={{ fontSize: '1.25rem' }} />,
 	},
 	{
 		id: 5,
@@ -824,7 +815,7 @@ export const LinksPage = [
 		pathname: "/positions",
 		href: "/positions",
 		title: "Ir a página de posiciones",
-		icon: <PositionsIcon className="" />,
+		icon: <i className="pi pi-sort-numeric-down" style={{ fontSize: '1.25rem' }} />,
 	},
 	{
 		id: 6,
@@ -832,7 +823,7 @@ export const LinksPage = [
 		pathname: "/statistics",
 		href: `/statistics?tournament=${new Date().getMonth() < 6 ? "0168" : "0159"}&type=goals`,
 		title: "Ir a página de estadística",
-		icon: <StatsIcon className="" />,
+		icon: <i className="pi pi-chart-bar" style={{ fontSize: '1.25rem' }} />,
 	},
 ]
 
