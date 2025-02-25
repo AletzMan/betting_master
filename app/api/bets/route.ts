05d1cff86f611f586cd7f8101816f847afefbc2d
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-02-25 15:39:15 -0600

    refactor: Add API endpoints for retrieving and creating bets

---
+++ app/api/bets/route.ts
@@ -0,0 +1,65 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/app/utils/db";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { ConflictError, DefaultError, NotFoundError, ServerError, UnprocessableEntityError } from "../_services/errors";
+import { SuccessCreate, SuccessResponse } from "../_services/successfulResponses";
+import { UserSchema } from "@/app/validations/userSchema";
+import { ZodError } from "zod";
+import { BetSchema } from "@/app/validations/betSchema";
+
+export async function GET(request: NextRequest) {
+
+    const response = await prisma?.bet.findMany()
+
+    if (response) {
+        return SuccessResponse(response);
+    }
+    return NotFoundError();
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const data = await request.json()
+        // 1. Validar los datos de la apuesta
+        const validatedBetData = BetSchema.parse(data);
+
+        // 2. Crear las Predictions
+        const createdPredictions = await Promise.all(
+            validatedBetData.predictions.map(async (prediction) => {
+                return prisma.prediction.create({
+                    data: {
+                        prediction: prediction,
+                    },
+                });
+            })
+        );
+
+        // 3. Obtener los IDs de las Predictions creadas
+        const predictionIds = createdPredictions.map((prediction) => prediction.id);
+
+        // 4. Crear la Bet con los predictionIds
+        const newBet = await prisma.bet.create({
+            data: {
+                ...validatedBetData,
+                paid: false,
+                predictionIds: predictionIds,
+            },
+        });
+        if (newBet) {
+            return SuccessCreate(newBet);
+        }
+        return DefaultError("Error al crear el registro");
+
+    } catch (error) {
+        console.error(error);
+        if (error instanceof PrismaClientKnownRequestError) {
+            if (error.code === "P2002") {
+                return ConflictError();
+            }
+        }
+        if (error instanceof ZodError) {
+            return UnprocessableEntityError(error.issues);
+        }
+        return ServerError();
+    }
+}
---
+++ app/validations/betSchema.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+
+export const BetSchema = z.object({
+    uid: z.string().min(1, "UID es requerido"),
+    day: z.string().min(1, "Día es requerido"),
+    name: z.string().min(1, "Nombre es requerido"),
+    predictions: z.array(z.enum(["L", "V", "E"])).min(1, "Se requiere al menos una predicción"), // Validación de predictions
+    matches: z.string().array(), // Array de strings
+    season: z.string().min(1, "Temporada es requerido"),
+    tournament: z.string().min(1, "Torneo es requerido"),
+});
