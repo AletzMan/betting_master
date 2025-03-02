a370618be7a4c4704ea1498ff96f17adaaed9750
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-03-02 01:27:05 -0600

    refactor: Add userInfoId to bet data in POST request

--- app/api/bets/route.ts
+++ app/api/bets/route.ts
@@ -69,6 +69,7 @@ export async function POST(request: NextRequest) {
             data: {
                 ...betDataWithoutPredictions,
                 paid: false,
+                userInfoId: betDataWithoutPredictions.uid,
                 predictionIds: predictionIds,
             },
         });
