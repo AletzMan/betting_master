31c4a87b0710ee7b288f47eb94e3cba78d9171f5
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-03-04 16:58:13 -0600

    refactor: Update HeaderMatches component to use IMatch interface and improve code readability

--- app/bets/components/HeaderMatches/HeaderMatches.tsx
+++ app/bets/components/HeaderMatches/HeaderMatches.tsx
@@ -1,10 +1,9 @@
-import { FinishedIcon, UnstartedIcon } from "@/svg"
-import styles from "./headermatches.module.scss"
-import { ICurrentMatch } from "@/types/types"
+import { IMatch } from "@/types/types"
 import { TeamsLogos } from "@/constants/constants"
+import React from "react"
 
 interface PropsHeaderMatches {
-	match: ICurrentMatch
+	match: IMatch
 	index: number
 }
 
@@ -15,20 +14,19 @@ export function HeaderMatches(props: PropsHeaderMatches) {
 		<>
 			{match &&
 				<li
-					className={`
-			${styles.match} 
-			${match.status === "Finalizado" && styles.matchEnd}  
-			${match.status === "En juego" && styles.matchLive}
-			${match.status === "Sin comenzar" && styles.matchUnstarted}`}
+					className={`relative flex flex-col items-center justify-center min-w-10 overflow-hidden w-full h-24 rounded-sm
+			${match.status === "finished" && "bg-(--finished-color)"}  
+			${match.status === "in progress" && "bg-(--started-color)"}
+			${match.status === "not started" && "bg-(--pending-color)"}`}
 				>
-					{TeamsLogos[match.teams.home].logo}
-					<span className={`${styles.matchName} ${styles.matchNameHome}`}>{TeamsLogos[match.teams.home].abbName}</span>
-					<span className={styles.matchVS}>VS</span>
-					{TeamsLogos[match.teams.away].logo}
-					<span className={`${styles.matchName} ${styles.matchNameAway}`}>{TeamsLogos[match.teams.away].abbName}</span>
-					{match.status === "Finalizado" && <FinishedIcon className={styles.matchStatus} />}
-					{match.status === "En juego" && <div className={`${styles.matchStatus} ${styles.matchStatusLive}`}></div>}
-					{match.status === "Sin comenzar" && <UnstartedIcon className={styles.matchStatus} />}
+					{React.cloneElement(TeamsLogos.find(team => team.id.toString() === match.homeTeam)?.logo as React.ReactElement, { className: "absolute -top-2 -left-2 w-7 h-7 object-fit" })}
+					<span className="text-xs font-bold">{TeamsLogos.find(team => team.id.toString() === match.homeTeam)?.abbName}</span>
+					<span className="">vs</span>
+					<span className="text-xs font-bold">{TeamsLogos.find(team => team.id.toString() === match.awayTeam)?.abbName}</span>
+					{React.cloneElement(TeamsLogos.find(team => team.id.toString() === match.awayTeam)?.logo as React.ReactElement, { className: "absolute -bottom-2 -right-2 w-7 h-7 object-fit" })}
+					{match.status === "finished" && <i className="pi pi-check-circle absolute top-0.5 right-0.5 text-green-300" style={{ fontSize: "0.9em" }} />}
+					{match.status === "in progress" && <i className="pi pi-circle-fill absolute top-0.5 right-0.5 text-red-500 animate-ping" style={{ fontSize: "0.9em" }} />}
+					{match.status === "not started" && <i className="pi pi-clock absolute top-0.5 right-0.5 text-yellow-300" style={{ fontSize: "0.9em" }} />}
 				</li>
 			}
 		</>
